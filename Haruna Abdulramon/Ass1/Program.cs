f632d5b15b3f2ae2e98b417a6d28262a9999c339
Ees18190253 <87358517+Ees18190253@users.noreply.github.com>
2021-07-13 08:15:16 +0100

    Add files via upload

---
+++ Haruna Abdulramon/Ass1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome\nPls enter your program");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "full time"){
+                Console.WriteLine("Enter your course");
+                Console.Write(Console.ReadLine() + " is a good course");
+            } else if (userInput == "part time"){
+                Console.WriteLine("How many credit are you taking?");
+                int UserCreditNo = int.Parse(Console.ReadLine());
+                if (UserCreditNo > 6) {
+                    Console.WriteLine("Too much unit for a part-time student");
+                    if (UserCreditNo <= 6) {
+                        Console.WriteLine("proceed to screen");
+                    } else {
+                        Console.WriteLine("You enter the wrong input");
+                    }
+
+                } else {
+                    Console.WriteLine("proceed to screen");
+                }
+
+            }
+        }
+    }
+}
